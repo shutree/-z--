eff377923d85a50967919d90b4b309e420d19708
dapang <771029485@qq.com>
2018-08-19 21:16:02 +0800

    数据库连接成功获取数据,商品列表页

--- src/api/goods.php
+++ src/api/goods.php
@@ -12,12 +12,13 @@
         // 输出信息并结束连接
         die("连接失败: " . $conn->connect_error);
     }
-    $sql = "select * from goods";
+    $conn->set_charset('utf8');
+    $sql = "select * from goodslist";
 
     // 执行sql语句
     $result = $conn->query($sql);
+    $row = $result->fetch_all(MYSQLI_ASSOC);
+    // print_r($row);
 
-
-    
-
+    echo json_encode($row,JSON_UNESCAPED_UNICODE);
 ?>
---
+++ src/js/product.js
@@ -0,0 +1,38 @@
+
+// jQuery(function($){
+        
+
+//     });
+(function(){
+
+    var main = document.querySelector('.main-r-list');
+    var ul = main.children[0];
+    let status = [200,304];
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function(){
+        if(status.indexOf(xhr.status)>=0){
+            var data= JSON.parse(xhr.responseText);
+            cre(data);
+        }
+    }
+    xhr.open('get','../api/goods.php');
+    xhr.send(null);
+
+    function cre(data){
+        ul.innerHTML = data.map(function(item){
+            return `<li>
+                    <img src="${item.imgurl}" />
+                    <div>
+                    <p><span>￥</span><span class="pro_price"></span></p>
+                    <p><span>销量</span><span class="sales"></span></p>
+                    </div>
+                    <h5>${item.title}</h5>
+                    <p>${item.dealer}</p>
+                    <div>
+                    <span>加入收藏</span>
+                    <span>加入购物车</span>
+                    </div>
+                </li>`;
+        }).join('');
+    }
+})();
